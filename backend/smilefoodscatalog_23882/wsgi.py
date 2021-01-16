7680b8fbcbac448636157f7e0228ba23cf4fbb13
Crowdbotics <team@crowdbotics.com>
2021-01-16 16:24:19 +0000

    Update models

--- backend/smilefoodscatalog_23882/wsgi.py
+++ backend/smilefoodscatalog_23882/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'smilefoodscatalog_23882.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "smilefoodscatalog_23882.settings")
 
 application = get_wsgi_application()
